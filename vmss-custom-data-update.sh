b1bb3c32718968face59e8faa8101df48cc869aa
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-12 16:42:03 +0000

    updating required variables

--- vmss-custom-data-update.sh
+++ vmss-custom-data-update.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-# defaults
-export AZ_LOCATION=${AZ_LOCATION:-uksouth}
-export AZ_VMSS_VM_SKU=${AZ_VMSS_VM_SKU:-Standard_B2s}
-export AZ_VMSS_STORAGE_SKU=${AZ_VMSS_STORAGE_SKU:-Standard_LRS}
-export AZ_VMSS_ADMIN_NAME=${AZ_VMSS_ADMIN_NAME:-adminuser}
-export AZ_VMSS_INSTANCE_COUNT=${AZ_VMSS_INSTANCE_COUNT:-0}
-export AZ_VMSS_IDENTITY=${AZ_VMSS_IDENTITY:-[system]}
-export AZ_VMSS_IMAGE=${AZ_VMSS_IMAGE:-Canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest}
-export AZ_VMSS_BOOT_DIAGS_ENABLED=${AZ_VMSS_BOOT_DIAGS_ENABLED:-true}
-export AZ_VMSS_CLOUD_INIT=${AZ_VMSS_CLOUD_INIT:-cloud-init/cloud-init.yml}
+required_variables=("AZ_VMSS_CLOUD_INIT" "AZ_SUBSCRIPTION_ID" "AZ_VMSS_NAME" 
+"AZ_VMSS_RESOURCE_GROUP_NAME")
+
+for i in "${required_variables[@]}"
+do
+    printf "%s: %s\n" "$i" "${!i}"
+    if [[ -z "${!i}" ]]
+    then
+      echo "Value for $i cannot be empty"
+      exit 1
+    fi
+done
 
 # https://learn.microsoft.com/en-us/rest/api/compute/virtual-machine-scale-sets/update?tabs=HTTP
 
