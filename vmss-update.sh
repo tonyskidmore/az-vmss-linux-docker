fd0435d9498695ed6e1fde898eac559bc97ffec2
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-12 10:05:02 +0000

    fixing no-wait

--- vmss-update.sh
+++ vmss-update.sh
@@ -43,7 +43,7 @@ az rest --uri "$uri" \
 az vmss reimage --name "$AZ_VMSS_NAME" \
                 --resource-group "$AZ_VMSS_RESOURCE_GROUP_NAME" \
                 --subscription "$AZ_SUBSCRIPTION_ID" \
-                --nowait
+                --no-wait
 
 # az rest -m patch -b '{
 #   "properties": {
